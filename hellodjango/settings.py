e28c475274d5eba80f6f165e6bce83409c1503ea
twoodside <twoodside@unomaha.edu>
2014-10-06 01:56:11 -0500

    mylinks

--- hellodjango/settings.py
+++ hellodjango/settings.py
@@ -89,7 +89,15 @@ USE_TZ = True
 
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+DATABASES = {
+	'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+	}
+}
+dbconfig_x =  dj_database_url.config()
+if dbconfig_x:
+	DATABASES['default']  = dbconfig_x
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
