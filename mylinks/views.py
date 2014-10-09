40f8ec551e3fb25e407926d843bf9bdedbce64e2
twoodside <twoodside@unomaha.edu>
2014-10-09 14:27:31 -0500

    categoriedlinks

--- mylinks/views.py
+++ mylinks/views.py
@@ -3,7 +3,8 @@ import re;
 from django.shortcuts import render;
 from django.http import HttpResponse,HttpResponseRedirect;
 from django.template import RequestContext, loader;
-from django.core.urlresolvers import reverse
+from django.core.urlresolvers import reverse;
+from django.shortcuts import redirect;
 
 
 from mylinks.models import Category,Link;
@@ -81,52 +82,5 @@ def categoriesAddUpdateNum(request,stuff):
 			link.link_label=newLinks[i][0];
 			link.link_url=newLinks[i][1];
 			link.save();
-		
-		# if (linktype=="Link" or linktype=="URL"):
-			# print(":"+request.POST[x]+":");
-			# if ( p.link_set.filter(id=linkid).exists() ):
-				# link=p.link_set.get(id=linkid);
-				# if (linktype=="Link"):
-					# r+="1 "+request.POST[x]+"<br>";
-					# if ( link.link_label != request.POST[x] ):
-						# link.link_label=request.POST[x];
-				# elif (linktype=="URL"):
-					# r+="2 "+request.POST[x]+"<br>";
-					# if ( link.link_url != request.POST[x] ):
-						# link.link_url=request.POST[x];
-						
-				# link.save();
-			# else:
-				# r+="_";
-				# linkid=int(linkid);
-				# while ( len(newLinks) <= linkid ):
-					# r+='+';
-					# newLinks.append('\0');
-				# if (newLinks[linkid] == '\0'):
-					# link=Link();
-					# link.category=p;
-					# if (linktype=="Link"):
-						# r+="3 "+request.POST[x]+"<br>";
-						# link.link_label=request.POST[x];
-					# elif (linktype=="URL"):
-						# r+="4 "+request.POST[x]+"<br>";
-						# link.link_url=request.POST[x];
-					# newLinks[linkid]=link;
-				# else:
-					# link=newLinks[linkid];
-					# r+="-";
-					# if (linktype=="Link"):
-						# r+="5 "+request.POST[x]+"<br>";
-						# link.link_label=request.POST[x];
-					# elif (linktype=="URL"):
-						# r+="6 "+request.POST[x]+"<br>";
-						# link.link_url=request.POST[x];
-					# newLinks[linkid]=link;
-
-	# for link in newLinks:
-		# if (link != '\0'):
-			# link.save();
 	
-	#return HttpResponse(r);
-	#return index(request);
 	return redirect("index");
