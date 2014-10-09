0bead2bdf2130ed3f04628545a51ec62181b82f9
twoodside <twoodside@unomaha.edu>
2014-10-09 15:39:31 -0500

    mylinks

--- mylinks/urls.py
+++ mylinks/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns, url;
 from mylinks import views;
 
 urlpatterns = patterns( "",
-	url(r'^$', 'views.index', name='categoriedlinks_index_orig'),
+	url(r'^$', views.index, name='categoriedlinks_index_orig'),
 	url(r'^updated/$', views.index, name='categoriedlinks_index'),
 	url(r'^update/(?P<stuff>\d+)/$',views.updateNum,name='updateNum'),
 	url(r'^update/$',views.update,name='update'), 
